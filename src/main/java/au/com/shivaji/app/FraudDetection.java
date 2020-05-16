e36d28cdfd38817f5c5f9f8ab8f5fe421cb14fe2
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2020-05-17 01:16:45 +1000

    Random Data

--- src/main/java/au/com/shivaji/app/FraudDetection.java
+++ src/main/java/au/com/shivaji/app/FraudDetection.java
@@ -5,161 +5,50 @@ import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
 
+@Slf4j
 public class FraudDetection {
-
   private static final Map<LocalDate, Map<String, Double>> transactions = new HashMap<>();
 
-  //Map <TransactionDate, Map<Sum of transactions in the slots of 100, Map<Transaction Amount, Credit Card Number>>>
   public static void main(String[] args) {
-    List<String> entries = new ArrayList<>() {{
-      add("CC 1,2014-04-29T13:15:54,10.00");
-      add("CC 1,2014-04-29T13:15:54,20.30");
-      add("CC 1,2014-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-04-29T13:15:54,20.00");
-      add("CC 1,2017-04-29T13:15:54,10.00");
-      add("CC 1,2017-04-29T13:15:54,20.30");
-      add("CC 1,2017-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.10");
-      add("CC 3,2019-04-29T13:15:54,20.00");
-      add("CC 2,2019-04-29T13:15:54,20.00");
-      add("CC 1,2014-04-29T13:15:54,10.00");
-      add("CC 1,2014-04-29T13:15:54,20.30");
-      add("CC 1,2014-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-04-29T13:15:54,20.00");
-      add("CC 9,2017-04-29T13:15:54,10.00");
-      add("CC 9,2017-04-29T13:15:54,20.30");
-      add("CC 9,2017-04-29T13:15:54,20.00");
-      add("CC 8,2018-04-29T13:15:54,20.00");
-      add("CC 7,2018-04-29T13:15:54,20.00");
-      add("CC 6,2018-04-29T13:15:54,20.10");
-      add("CC 6,2019-04-29T13:15:54,20.00");
-      add("CC 5,2019-04-29T13:15:54,20.00");
-      add("CC 1,2014-04-29T13:15:54,10.00");
-      add("CC 1,2014-04-29T13:15:54,20.30");
-      add("CC 1,2014-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-04-29T13:15:54,20.00");
-      add("CC 1,2017-04-29T13:15:54,10.00");
-      add("CC 1,2017-04-29T13:15:54,20.30");
-      add("CC 1,2017-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.10");
-      add("CC 3,2019-04-29T13:15:54,20.00");
-      add("CC 2,2019-04-29T13:15:54,20.00");
-      add("CC 1,2014-04-29T13:15:54,10.00");
-      add("CC 1,2014-04-29T13:15:54,20.30");
-      add("CC 1,2014-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-04-29T13:15:54,20.00");
-      add("CC 9,2017-04-29T13:15:54,10.00");
-      add("CC 9,2017-04-29T13:15:54,20.30");
-      add("CC 9,2017-04-29T13:15:54,20.00");
-      add("CC 8,2018-04-29T13:15:54,20.00");
-      add("CC 7,2018-04-29T13:15:54,20.00");
-      add("CC 6,2018-04-29T13:15:54,20.10");
-      add("CC 6,2019-04-29T13:15:54,20.00");
-      add("CC 5,2019-04-29T13:15:54,20.00");
-      add("CC 1,2014-04-29T13:15:54,10.00");
-      add("CC 1,2014-04-29T13:15:54,20.30");
-      add("CC 1,2014-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-04-29T13:15:54,20.00");
-      add("CC 1,2017-04-29T13:15:54,10.00");
-      add("CC 1,2017-04-29T13:15:54,20.30");
-      add("CC 1,2017-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.10");
-      add("CC 3,2019-04-29T13:15:54,20.00");
-      add("CC 2,2019-04-29T13:15:54,20.00");
-      add("CC 1,2014-04-29T13:15:54,10.00");
-      add("CC 1,2014-04-29T13:15:54,20.30");
-      add("CC 1,2014-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-04-29T13:15:54,20.00");
-      add("CC 9,2017-04-29T13:15:54,10.00");
-      add("CC 9,2017-04-29T13:15:54,20.30");
-      add("CC 9,2017-04-29T13:15:54,20.00");
-      add("CC 8,2018-04-29T13:15:54,20.00");
-      add("CC 7,2018-04-29T13:15:54,20.00");
-      add("CC 6,2018-04-29T13:15:54,20.10");
-      add("CC 6,2019-04-29T13:15:54,20.00");
-      add("CC 5,2019-04-29T13:15:54,20.00");
-      add("CC 1,2014-04-29T13:15:54,10.00");
-      add("CC 1,2014-04-29T13:15:54,20.30");
-      add("CC 1,2014-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-04-29T13:15:54,20.00");
-      add("CC 1,2017-05-29T13:15:54,10.00");
-      add("CC 1,2017-06-29T13:15:54,20.30");
-      add("CC 1,2017-04-29T13:15:54,20.00");
-      add("CC 1,2018-07-29T13:15:54,20.00");
-      add("CC 1,2018-08-29T13:15:54,20.00");
-      add("CC 1,2018-04-29T13:15:54,20.10");
-      add("CC 3,2019-05-29T13:15:54,20.00");
-      add("CC 2,2019-04-29T13:15:54,20.00");
-      add("CC 1,2014-05-29T13:15:54,10.00");
-      add("CC 1,2014-05-29T13:15:54,20.30");
-      add("CC 1,2014-08-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-04-29T13:15:54,20.00");
-      add("CC 1,2013-09-29T13:15:54,20.10");
-      add("CC 3,2013-04-29T13:15:54,20.00");
-      add("CC 2,2013-01-29T13:15:54,20.00");
-      add("CC 9,2017-04-29T13:15:54,10.00");
-      add("CC 9,2017-03-29T13:15:54,20.30");
-      add("CC 9,2017-04-29T13:15:54,20.00");
-      add("CC 8,2018-04-29T13:15:54,20.00");
-      add("CC 7,2018-05-29T13:15:54,20.00");
-      add("CC 6,2018-04-29T13:15:54,20.10");
-      add("CC 6,2019-04-29T13:15:54,20.00");
-      add("CC 5,2019-07-29T13:15:54,20.00");
-    }};
+    int noOfRecordsToSample = 10000000; // 10 Million records
+    List<String> entries = Collections.synchronizedList(new ArrayList<>());
+    IntStream.range(0, noOfRecordsToSample).parallel().forEach((i) -> {
+      entries.add(StringUtils.joinWith(",", RandomStringUtils.randomAlphanumeric(10), randomDateTime(), RandomUtils.nextDouble(0, 20000)));
+    });
+    log.debug("Total of sample entries:: " + entries.size());
     var start = System.nanoTime();
     entries.forEach(FraudDetection::parse);
-    System.out.println("Parsing took::" + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + " milli seconds");
-    LocalDate date = LocalDate.parse("2019-04-29");
-    double amount = Double.parseDouble("15.23");
-//    System.out.println(transactions);
+    LocalDate date = LocalDateTime.parse(randomDateTime()).toLocalDate();
+    double amount = RandomUtils.nextDouble(0, 20000);
+    log.debug("For Date " + date + " Transaction amount " + amount);
     if (transactions.containsKey(date)) {
-      var ccs = transactions.get(date).entrySet().stream().filter(e -> e.getValue() >= amount).map(Map.Entry::getKey).collect(toList());
-      System.out.println(ccs);
+      Stream<Map.Entry<String, Double>> entryStream;
+      Map<String, Double> ccToTotalAmtMap = transactions.get(date);
+      if (ccToTotalAmtMap.size() > 10000) { // Many records do in parallel
+        entryStream = ccToTotalAmtMap.entrySet().parallelStream();
+      } else { //  Not enough records to run in parallel
+        entryStream = ccToTotalAmtMap.entrySet().stream();
+      }
+      var ccs = entryStream.filter(e -> e.getValue() >= amount).map(Map.Entry::getKey).collect(toList());
+      log.debug(ccs.size() + " in " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + " milli seconds");
+      ccs.parallelStream().forEach(cc -> log.debug(cc + " :: " + ccToTotalAmtMap.get(cc)));
     } else {
-      System.out.println("No Transactions");
+      log.debug("No Transactions in " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + " milli seconds");
     }
-    System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) + " milli seconds");
   }
 
   private static void parse(String line) {
@@ -180,6 +69,15 @@ public class FraudDetection {
       entries.put(creditCardNumberHash, (double) 0);
     }
     entries.put(creditCardNumberHash, entries.get(creditCardNumberHash) + transactionAmount);
+  }
+
+  public static String randomDateTime() {
+    LocalDateTime tmp = LocalDateTime.of(LocalDate.of(randBetween(1985, 2020), randBetween(1, 12), randBetween(1, 28)), LocalTime.now());
+    return tmp.toString();
+  }
+
+  public static int randBetween(int start, int end) {
+    return start + (int) Math.round(Math.random() * (end - start));
   }
 
 }
